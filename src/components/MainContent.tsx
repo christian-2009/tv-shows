f89be497653acafbc2333fbb5098e3376a5f2d44
Christian Sophocleous <c-sophocleous@hotmail.com>
2022-03-21 17:23:44 +0000

    used format

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,12 +1,12 @@
-import React from "react"
-import fixedSummaryEpisodes from "../utils/fixSummary"
-import EpisodeEntry from "./EpisodeEntry"
+import React from "react";
+import fixedSummaryEpisodes from "../utils/fixSummary";
+import EpisodeEntry from "./EpisodeEntry";
 
 export default function MainContent(): JSX.Element {
-    const episode = fixedSummaryEpisodes.map(EpisodeEntry)
-    return (
-        <>
-            <div>{episode}</div>
-        </>
-    )
+  const episode = fixedSummaryEpisodes.map(EpisodeEntry);
+  return (
+    <>
+      <div>{episode}</div>
+    </>
+  );
 }
