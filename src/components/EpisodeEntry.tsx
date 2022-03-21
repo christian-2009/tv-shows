2a08bb52c9a165b5f76ad4759359d663f26cc1de
Christian Sophocleous <c-sophocleous@hotmail.com>
2022-03-21 17:21:34 +0000

    all episodes on screen without <p>

---
+++ src/components/EpisodeEntry.tsx
@@ -0,0 +1,29 @@
+interface IEpisode {
+    id: number;
+    url: string;
+    name: string;
+    season: number;
+    number: number;
+    type: string;
+    airdate: string;
+    airtime: string;
+    airstamp: string;
+    runtime: number;
+    image: {
+      medium: string;
+      original: string;
+    };
+    summary: string;
+    _links: { self: { href: string } };
+  }
+
+export default function EpisodeEntry(props: IEpisode): JSX.Element {
+    return (
+        <>
+            <h1>{props.name}</h1>
+            <h3>{props.season}, {props.number}</h3>
+            <img src={props.image.medium} alt="GOT" />
+            <p>{props.summary}</p>
+        </>
+    )
+}
---
+++ src/utils/fixSummary.ts
@@ -0,0 +1,33 @@
+import episodes from '../episodes.json'
+
+interface IEpisode {
+    id: number;
+    url: string;
+    name: string;
+    season: number;
+    number: number;
+    type: string;
+    airdate: string;
+    airtime: string;
+    airstamp: string;
+    runtime: number;
+    image: {
+      medium: string;
+      original: string;
+    };
+    summary: string;
+    _links: { self: { href: string } };
+  }
+
+
+
+function fixSummary(array: IEpisode[]){
+    const newArr = []
+    for (const object of array){
+      object.summary = object.summary.slice(3,object.summary.length -4)
+      newArr.push(object)
+    }return newArr
+  }
+  
+const fixedSummaryEpisodes = fixSummary(episodes)
+export default fixedSummaryEpisodes
